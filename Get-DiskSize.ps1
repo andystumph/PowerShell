bb3eeee8cc3f505e4007e21f3edda4eacb8a3722
andystumph <andy.stumph@gmail.com>
2016-09-23 14:36:38 -0400

    Add Get-DiskSize

---
+++ Get-DiskSize.ps1
@@ -0,0 +1,60 @@
+    
+<#  
+.SYNOPSIS  
+    Retrieves Disk and space information from  a computer.
+.DESCRIPTION
+    Retrieves Disk and space information from  a computer.
+.PARAMETER computer
+    Name of computer to get disk information on.
+.NOTES  
+    Name: Get-DiskSize
+    Author: Andy Stumph
+    DateCreated: 12-12-2014
+.EXAMPLE
+    .\Get-DiskSize.ps1 -computer ServerName
+
+#> 
+
+# Requires -version 3.0
+
+[cmdletbinding()]
+
+    Param(
+    [Parameter(Mandatory = $False, Position = 0, ValueFromPipeline = $True)]
+    [string]$Computer = 'localhost'
+    )
+
+$Output = @()
+## DCOM for older OS's.
+$Option = New-CimSessionOption -Protocol Dcom
+$Session = New-CimSession -SessionOption $Option -ComputerName $Computer
+
+$LogicalDisks = Get-CimInstance -CimSession $Session -ClassName Win32_LogicalDisk`
+
+foreach ($LogicalDisk in $LogicalDisks) {
+
+    $Type = $LogicalDisk.DriveType
+    Switch ($Type)
+     {
+         2 {$Type="FDD"}
+         3 {$Type="HDD"}
+	     4 {$Type="NET"}
+         5 {$Type="CD "}
+     }
+
+     If ($LogicalDisk.Size / 1MB -ne 0.00) {$FreeSpacePercent = (($LogicalDisk.FreeSpace / 1MB) / ($LogicalDisk.Size / 1MB))} else {$FreeSpacePercent = 0}
+
+     $Disk = [pscustomobject]@{
+        
+        "Device ID" = $LogicalDisk.DeviceID
+        Type = $Type
+        "Size (MB)" = "{0:N0}" -f $($LogicalDisk.Size / 1MB)
+        "Free Space (MB)" = "{0:N0}" -f $($LogicalDisk.FreeSpace / 1MB)
+        "Percent Free" = "{0:P0}" -f $FreeSpacePercent
+     }
+     
+     $Output += $Disk
+}     
+
+$Output | Format-Table
+
